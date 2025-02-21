295937ccb067203e2508606eed3cd4f082357912
Sergey <hsv79ya@gmail.com>
2025-02-21 15:13:25 +0500

    Init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tetris 2D on Tile Map Layers
+
+Easy Tetris 2D - written in Godot 4.3
+
+Features
+ - add a quick figure landing
+
+TODO
+ - add all standart figures
+ - add figure rotation
+ - add scoring
+ - add full line deletion
+ - add an increase in falling speed
---
+++ assets/scripts/figure.gd
@@ -0,0 +1,76 @@
+extends CharacterBody2D
+
+const SPEED = 1000.0
+const GRAVITY = 10000
+
+var is_new_figure = true
+var complete = true
+var is_rapid = false
+
+const START_POSITION = Vector2i(560, 0)
+
+@onready var cube = $Cube
+@onready var line = $Line
+@onready var hook = $Hook
+
+var figures = []
+var actual_figure = null
+
+func _ready() -> void:
+	figures.append(cube)
+	figures.append(line)
+	figures.append(hook)
+
+	create_new_figure()
+
+func _physics_process(delta: float) -> void:
+	if complete:
+		return
+
+	if Input.is_action_pressed("quick_down"):
+		velocity.y += GRAVITY * delta * 5
+
+	if not is_on_floor() and is_new_figure:
+		is_new_figure = false
+	#	#velocity += get_gravity() * delta
+		velocity.y += GRAVITY * delta
+
+	if is_on_floor():
+		print("Need create new figure")
+		create_new_figure()
+
+	if Input.is_action_pressed("ui_down"):
+		if !is_rapid and !complete:
+			is_rapid = true
+			velocity.y += GRAVITY * delta * 2
+
+	if Input.is_action_just_released("ui_down"):
+		if is_rapid and !complete:
+			is_rapid = false
+			velocity.y -= GRAVITY * delta * 2
+
+	var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+	#	velocity.x = direction * SPEED
+	#else:
+	#	velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction:
+		if direction < 0:
+			position.x -= delta * SPEED
+		else:
+			position.x += delta * SPEED
+
+	move_and_slide()
+
+func create_new_figure() -> void:
+	position = START_POSITION
+	if actual_figure:
+		actual_figure.disabled = true
+		actual_figure.visible = false
+	actual_figure = figures[randi() % figures.size()]
+	actual_figure.disabled = false
+	actual_figure.visible = true
+	is_new_figure = true
+	is_rapid = false
+	velocity.y = 0
+	complete = false
